035d947512cf626aaa66787b739e0ac8bdad1555
Dani Santos <santos.adaniele@gmail.com>
2020-02-05 12:00:41 -0500

    exercise1-2

--- workshop/exercise-1/exercise-2/scripts.js
+++ workshop/exercise-1/exercise-2/scripts.js
@@ -37,6 +37,8 @@ const img = document.createElement("img");
 
 mainDiv.appendChild(img);
 
+img.classList.add("image");
+
 img.setAttribute("src", "./images/robin-sparkles.jpg");
 
 img.setAttribute("alt", "Robin Sparkles");
@@ -45,6 +47,8 @@ const paragraph3 = document.createElement("p");
 
 mainDiv.appendChild(paragraph3);
 
+paragraph3.classList.add("paragraph");
+
 paragraph3.innerText =
   "In this episode, Ted is learning a few secrets about Robin, namely that she has an unexplained aversion to the mall. Robin refuses to tell Ted and the others why she won’t go to the mall, so the gang forms their own theories. Marshall believes it’s because she got married in a mall and is still married. Barney believes it has something to do with Robin having performed in a porn video. The two of them make a slap bet with each other: whoever is right gets to slap the other across the face as hard as he can. Lily is named the Slap Bet Commissioner, as long as she promises to be unbiased.";
 
@@ -52,5 +56,20 @@ const paragraph4 = document.createElement("p");
 
 mainDiv.appendChild(paragraph4);
 
+paragraph4.classList.add("paragraph");
+
 paragraph4.innerText =
   "In the end, Robin’s big secret was revealed- she was a Canadian, teen pop sensation named Robin Sparkles. Barney finds the music video for her hit single “Lets Go To The Mall”. The slap bet takes a few turns throughout the episode, but ends with Marshall having the right to slap Barney 5 times- available for all of eternity (horrible call Barney).";
+
+const anchor = document.createElement("a");
+
+mainDiv.appendChild(anchor);
+
+anchor.classList.add("link");
+
+anchor.innerText = "Source";
+
+anchor.setAttribute(
+  "href",
+  "https://himym2017.wordpress.com/2017/03/25/the-best-himym-episode-according-to-fans/"
+);
