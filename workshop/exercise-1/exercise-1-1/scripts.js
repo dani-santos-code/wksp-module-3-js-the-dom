37ee9883f04bc5508e4d20b2c1b14d5c6aae9284
Dani Santos <santos.adaniele@gmail.com>
2020-02-05 11:38:42 -0500

    exercise 1- Part 1

--- workshop/exercise-1/exercise-1-1/scripts.js
+++ workshop/exercise-1/exercise-1-1/scripts.js
@@ -1,1 +1,48 @@
-console.log('...crickets...');
+const mainDiv = document.querySelector("#main");
+
+const h1 = document.createElement("h1");
+
+mainDiv.appendChild(h1);
+
+h1.innerText = "The best How I Met Your Mother episode (according to fans)";
+
+const paragraph = document.createElement("p");
+
+mainDiv.appendChild(paragraph);
+
+paragraph.innerText =
+  "As impossible as it seems to name just 1 of the 208 legendary episodes as the “best”, the fans have spoken and the highest rated episode is… wait for it…";
+
+const h2 = document.createElement("h2");
+
+mainDiv.appendChild(h2);
+
+h2.innerText = "The Slap Bet (Season 2, Episode 9)";
+
+const paragraph2 = document.createElement("p");
+
+mainDiv.appendChild(paragraph2);
+
+paragraph2.innerText = "IMDB Rating: 9.5";
+
+const img = document.createElement("img");
+
+mainDiv.appendChild(img);
+
+img.setAttribute("src", "./images/robin-sparkles.jpg");
+
+img.setAttribute("alt", "Robin Sparkles");
+
+const paragraph3 = document.createElement("p");
+
+mainDiv.appendChild(paragraph3);
+
+paragraph3.innerText =
+  "In this episode, Ted is learning a few secrets about Robin, namely that she has an unexplained aversion to the mall. Robin refuses to tell Ted and the others why she won’t go to the mall, so the gang forms their own theories. Marshall believes it’s because she got married in a mall and is still married. Barney believes it has something to do with Robin having performed in a porn video. The two of them make a slap bet with each other: whoever is right gets to slap the other across the face as hard as he can. Lily is named the Slap Bet Commissioner, as long as she promises to be unbiased.";
+
+const paragraph4 = document.createElement("p");
+
+mainDiv.appendChild(paragraph4);
+
+paragraph4.innerText =
+  "In the end, Robin’s big secret was revealed- she was a Canadian, teen pop sensation named Robin Sparkles. Barney finds the music video for her hit single “Lets Go To The Mall”. The slap bet takes a few turns throughout the episode, but ends with Marshall having the right to slap Barney 5 times- available for all of eternity (horrible call Barney).";
