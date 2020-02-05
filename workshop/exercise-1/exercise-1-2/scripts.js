a2bcf7d656f52fc9e47cebf4ca3a14e61496d8eb
Dani Santos <santos.adaniele@gmail.com>
2020-02-05 11:50:39 -0500

    fixes exercise 1-1

--- workshop/exercise-1/exercise-1-2/scripts.js
+++ workshop/exercise-1/exercise-1-2/scripts.js
@@ -4,11 +4,15 @@ const h1 = document.createElement("h1");
 
 mainDiv.appendChild(h1);
 
+h1.classList.add("title");
+
 h1.innerText = "The best How I Met Your Mother episode (according to fans)";
 
 const paragraph = document.createElement("p");
 
 mainDiv.appendChild(paragraph);
+
+paragraph.classList.add("paragraph");
 
 paragraph.innerText =
   "As impossible as it seems to name just 1 of the 208 legendary episodes as the “best”, the fans have spoken and the highest rated episode is… wait for it…";
@@ -17,11 +21,15 @@ const h2 = document.createElement("h2");
 
 mainDiv.appendChild(h2);
 
+h2.classList.add("subtitle");
+
 h2.innerText = "The Slap Bet (Season 2, Episode 9)";
 
 const paragraph2 = document.createElement("p");
 
 mainDiv.appendChild(paragraph2);
+
+paragraph2.classList.add("paragraph");
 
 paragraph2.innerText = "IMDB Rating: 9.5";
 
@@ -46,14 +54,3 @@ mainDiv.appendChild(paragraph4);
 
 paragraph4.innerText =
   "In the end, Robin’s big secret was revealed- she was a Canadian, teen pop sensation named Robin Sparkles. Barney finds the music video for her hit single “Lets Go To The Mall”. The slap bet takes a few turns throughout the episode, but ends with Marshall having the right to slap Barney 5 times- available for all of eternity (horrible call Barney).";
-
-const anchor = document.createElement("a");
-
-mainDiv.appendChild(anchor);
-
-anchor.innerText = "Source";
-
-anchor.setAttribute(
-  "href",
-  "https://himym2017.wordpress.com/2017/03/25/the-best-himym-episode-according-to-fans/"
-);
